a35820147e8e2663799ff905cf99e8b7a4a29d5d
jp <137669276@qq.com>
2024-07-26 11:53:57 +0800

    feat pont-case(init)

---
+++ packages/pont-case/build.config.ts
@@ -0,0 +1,19 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  // If entries is not provided, will be automatically inferred from package.json
+  entries: [
+    './src/index.ts',
+  ],
+
+  // Change outDir, default is 'dist'
+  outDir: 'dist',
+
+  clean: true,
+  // Generates .d.ts declaration file
+  declaration: true,
+  rollup: {
+    emitCJS: false,
+  },
+  failOnWarn: false,
+})
